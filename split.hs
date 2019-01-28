375235a595a725084b6e99f790bb11e9b17173f4
Gabriel Wohlford <GabrielWohlford@gmail.com>
2019-01-28 07:16:22 -0700

    Update split.hs

--- split.hs
+++ split.hs
@@ -6,10 +6,10 @@ split (x:xs) delim base = solution
   where
     tuple = break (==delim) (x:xs)
     tuplength = (length (snd tuple)) 
-    v1 = fst $ tuple 
+    v1 = fst tuple 
     mystr
       | tuplength > 0 = tail $ snd tuple 
-      | otherwise = fst tuple
+      | otherwise = v1
     solution
       | tuplength > 0 = base++split mystr delim ([v1]) 
       | otherwise = base++split mystr delim [[]]
