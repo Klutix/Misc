d79b37ad30d73976e595226f930b69233c497521
Gabriel Wohlford <GabrielWohlford@gmail.com>
2019-08-30 09:21:25 -0600

    Removed garbage text from a paste..

--- toBase.hs
+++ toBase.hs
@@ -4,6 +4,3 @@ toBase::Int->Int->String
 toBase v b
    | v > 0 = toBase (v`div`b) b ++ ["0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"!!(v `mod` b)]
    | otherwise = ""
-This paste expires on 2019-08-28 04:38:32. View raw. Remove now. Pasted through web.
-
-View source code, the removal or expiry stories, or read the about page.
